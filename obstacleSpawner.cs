9ff7f9c2faea4ee1bce9107a4078db696443dcab
Adebayo Olamilekan <75235148+Oleanji@users.noreply.github.com>
2021-07-11 06:29:14 +0100

    Update obstacleSpawner.cs

--- obstacleSpawner.cs
+++ obstacleSpawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class obstacleSpawner : MonoBehaviour
 {
-    public GameObject obstacle;
+    public GameObject obstacle; 
     // Start is called before the first frame update
     void Start()
     {
